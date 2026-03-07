438b9029b09040b4675cffdf9adac82453e0ef10
Igor Tudisco <igortudisco@hotmail.com>
2026-03-07 14:47:41 -0300

    Add tests for invalid dates and zero price in OfertaViagem
    Added two unit tests to OfertaViagemTest: one to validate that offers with a return date earlier than the departure date are invalid, and another to ensure offers with a price of zero are flagged as invalid. Both tests check for the correct error messages.

--- test/JornadaMilhas.Test/OfertaViagemTest.cs
+++ test/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -33,5 +33,34 @@ namespace JornadaMilhas.Test
             Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
             Assert.False(oferta.EhValido);
         }
+
+        [Fact]
+        public void TestandoOpertaDataVoltaMenor()
+        {
+            // Seguindo o padrão AAA (Arrange, Act, Assert)
+            Rota rota = new Rota("São Paulo", "Rio de Janeiro");
+            Periodo periodo = new Periodo(new DateTime(2026, 1, 15), new DateTime(2024, 10, 20));
+            double valor = 500.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, valor);
+
+            Assert.Contains("Erro: Data de ida não pode ser maior que a data de volta.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
+
+        [Fact]
+        public void TestandoOpertaValorZerado()
+        {
+            // Cenário - Arrange
+            Rota rota = new Rota("São Paulo", "Rio de Janeiro");
+            Periodo periodo = new Periodo(new DateTime(2024, 1, 15), new DateTime(2024, 1, 20));
+            double valor = 00.0;
+
+            // Ação - Act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, valor);
+
+            // Verificação - Assert
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        }
     }
 }
