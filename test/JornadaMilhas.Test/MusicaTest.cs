5b39d15ef4406a960bd85fa22e38b7b539ce06bf
Igor Tudisco <igortudisco@hotmail.com>
2026-03-07 15:07:42 -0300

    Add Musica class and unit tests for properties and ToString
    Introduced the Musica class with Name, Id, and Artist properties, including a constructor, ExibirFichaTecnica method, and ToString override. Added MusicaTest with unit tests to verify property initialization and ToString output.

---
+++ test/JornadaMilhas.Test/MusicaTest.cs
@@ -0,0 +1,53 @@
+﻿using JornadaMilhas.Modelos;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class MusicaTest
+    {
+        [Fact]
+        public void TesteNomeInicializadoCorretamente()
+        {
+            // Arrange
+            string nome = "Música Teste";
+
+            // Act
+            Musica musica = new Musica(nome);
+
+            // Assert
+            Assert.Equal(nome, musica.Nome);
+        }
+
+
+        [Fact]
+        public void TesteIdInicializadoCorretamente()
+        {
+            // Arrange
+            string nome = "Música Teste";
+            int id = 13;
+
+            // Act
+            Musica musica = new Musica(nome) { Id = id };
+
+            // Assert
+            Assert.Equal(id, musica.Id);
+        }
+
+        [Fact]
+        public void TesteToString()
+        {
+            // Arrange
+            int id = 1;
+            string nome = "Música Teste";
+            Musica musica = new Musica(nome);
+            musica.Id = id;
+            string toStringEsperado = @$"Id: {id} Nome: {nome}";
+
+            // Act
+            string resultado = musica.ToString();
+
+            // Assert
+            Assert.Equal(toStringEsperado, resultado);
+        }
+    }
+}
