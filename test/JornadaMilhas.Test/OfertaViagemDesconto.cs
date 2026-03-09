2fd61cde78eda5cdd7b9542068c5621e66133291
Igor Tudisco <igortudisco@hotmail.com>
2026-03-08 21:53:40 -0300

    Add discount property to OfertaViagem and related test
    Added a Desconto property to OfertaViagem that updates the Preco when set. Introduced a unit test (OfertaViagemDesconto) to verify that applying a discount correctly adjusts the price.

---
+++ test/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,28 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualQuandoAplicadoDesconto() // Usando TDD
+        {
+            Rota rota = new Rota("São Paulo", "Rio de Janeiro");
+            Periodo periodo = new Periodo(new DateTime(2024, 1, 15), new DateTime(2024, 1, 20));
+            double precoOriginal = 1000.0;
+            double desconto = 20.0;
+            double precoComDesconto = precoOriginal - desconto;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            oferta.Desconto = desconto;
+
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+    }
+}
