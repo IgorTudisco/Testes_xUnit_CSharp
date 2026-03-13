5564db585abd605e113e1537fa2d51303d072f9e
Igor Tudisco <igortudisco@hotmail.com>
2026-03-13 20:03:52 -0300

    Add integration test for OfertaViagemDAL RecuperarPorId
    Introduced OfertaViagemDalRecuperaPorId test class using IClassFixture to share database context. Added test to verify RecuperarPorId returns null for invalid ID (-1). Constructor logs database connection hash for debugging.

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDalRecuperaPorId.cs
@@ -0,0 +1,36 @@
+﻿using JornadaMilhas.Dados;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao;
+
+public class OfertaViagemDalRecuperaPorId : IClassFixture<ContextoFixture>
+{
+    private readonly JornadaMilhasContext _context;
+
+    public OfertaViagemDalRecuperaPorId(ITestOutputHelper outputHelper, ContextoFixture contextoFixture)
+    {
+        // Utilizando a conexão compartilhada para teste de percistencia
+        _context = contextoFixture.Context;
+        outputHelper.WriteLine($"Conexão {_context.GetHashCode()} com o banco de dados estabelecida com sucesso.");
+    }
+
+    [Fact]
+    public void RecuperaOfertaViagemPorId()
+    {
+        // arrage
+        var dal = new OfertaViagemDAL(_context);
+
+        // act
+        var oferta = dal.RecuperarPorId(-1);
+
+        // assert
+        Assert.Null(oferta);
+    }
+
+}
