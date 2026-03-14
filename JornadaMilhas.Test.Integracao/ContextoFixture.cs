847a0a5258892ada1acd5c18676a20ab646b79c9
Igor Tudisco <igortudisco@hotmail.com>
2026-03-14 15:50:18 -0300

    Use SQL Server Testcontainer for integration tests
    Replaced localdb with SQL Server container in ContextoFixture.
    Added Testcontainers.MsSql package to project.
    Introduced RecuperarTodasOfertaViagem integration test class.

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao;
 
@@ -12,10 +13,15 @@ public class ContextoFixture
 {
     public JornadaMilhasContext Context { get; }
 
+    // Configuração do container do SQL Server para testes de integração no Docker
+    private readonly MsSqlContainer _sqlContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .Build();
+
     public ContextoFixture()
     {
         var opitions = new DbContextOptionsBuilder<JornadaMilhasContext>()
-            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+            .UseSqlServer(_sqlContainer.GetConnectionString())
             .Options;
 
         Context = new JornadaMilhasContext(opitions);
---
+++ JornadaMilhas.Test.Integracao/RecuperarTodasOfertaViagem.cs
@@ -0,0 +1,31 @@
+﻿using JornadaMilhas.Dados;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao;
+
+[Collection(nameof(ContextoCollection))]
+public class RecuperarTodasOfertaViagem
+{
+    private readonly JornadaMilhasContext _context;
+
+    public RecuperarTodasOfertaViagem(ITestOutputHelper outputHelper, ContextoFixture contextoFixture)
+    {
+        // Utilizando a conexão compartilhada para teste de percistencia
+        _context = contextoFixture.Context;
+        outputHelper.WriteLine($"Conexão {_context.GetHashCode()} com o banco de dados estabelecida com sucesso.");
+    }
+
+    [Fact]
+    public void RecuperaTodasAsOfertasViagem()
+    {
+        // arrage
+        var dal = new OfertaViagemDAL(_context);
+
+        // act
+        var ofertas = dal.RecuperarTodas();
+
+        // assert
+        Assert.NotNull(ofertas);
+    }
+}
