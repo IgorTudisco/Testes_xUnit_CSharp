c80240c07e650c6fbcfa363fa9750b69c210a421
Igor Tudisco <igortudisco@hotmail.com>
2026-03-14 16:12:16 -0300

    Refactor ContextoFixture for async test setup/teardown
    ContextoFixture now implements IAsyncLifetime for proper async initialization and disposal of the SQL Server container. The constructor was replaced with InitializeAsync and DisposeAsync methods. Context property is now private set. This improves reliability and fixes issues with async context creation in integration tests.

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -9,21 +9,29 @@ using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao;
 
-public class ContextoFixture
+// fix: A arumando o problema de criação assíncrona do contexto
+public class ContextoFixture : IAsyncLifetime
 {
-    public JornadaMilhasContext Context { get; }
+    public JornadaMilhasContext Context { get; private set; }
 
     // Configuração do container do SQL Server para testes de integração no Docker
     private readonly MsSqlContainer _sqlContainer = new MsSqlBuilder()
         .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
         .Build();
 
-    public ContextoFixture()
+    public async Task InitializeAsync()
     {
+        _sqlContainer.StartAsync().Wait(); // Inicia o container do SQL Server
         var opitions = new DbContextOptionsBuilder<JornadaMilhasContext>()
             .UseSqlServer(_sqlContainer.GetConnectionString())
             .Options;
 
         Context = new JornadaMilhasContext(opitions);
+        Context.Database.Migrate(); // Aplica as migrate para criar o banco de dados e as tabelas necessárias para os testes
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _sqlContainer.StopAsync(); // Encerra o container do SQL Server após os testes
     }
 }
